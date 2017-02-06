0e451ab5b110879c3de8e1eb7c02acee225f48e7
Vincent Garrigues <vincent.garrigues@gmail.com>
2017-02-07 00:06:38 +0100

    improve command documentation

--- Sources/root.swift
+++ Sources/root.swift
@@ -8,16 +8,16 @@ let templatesPath = "/usr/src/ios-viper/Templates"
 let templatesPath = "Templates"
 #endif
 
-let rootCommand = Command(usage: "ios-viper", configuration: configuration, run: execute)
-
-private func configuration(command: Command) {
-    command.add(flags: [
-        Flag(shortName: "m", longName: "module", type: String.self, description: "the name of the module", required: true),
-        Flag(shortName: "t", longName: "type", type: String.self, description: "the type of the module", required: true),
-        Flag(shortName: "d", longName: "directory", type: String.self, description: "the directory in which to create the module", required: true)
-        ]
-    )
-}
+let rootFlags = [
+    Flag(shortName: "m", longName: "module", type: String.self, description: "The name of the module", required: true),
+    Flag(shortName: "t", longName: "type", type: String.self, description: "The type of the module", required: true),
+    Flag(shortName: "d", longName: "directory", type: String.self, description: "The directory in which to create the module", required: true)
+]
+let rootCommand = Command(usage: "ios-viper",
+                          shortMessage: "(B)VIPER module template generator",
+                          flags: rootFlags,
+                          example: "  ios-viper -m MyModule -t UIView -d /path/to/module",
+                          run: execute)
 
 private func execute(flags: Flags, args: [String]) {
     let module = flags.getString(name: "module")!
